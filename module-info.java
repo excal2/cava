d1e0b8c026866587094ca4c31c741cb0081ec571
Donal Munnelly <donal.munnelly@gmail.com>
2022-04-07 14:31:58 +0100

    initial upload
    adding main files

---
+++ module-info.java
@@ -0,0 +1,8 @@
+module test {
+	exports src.main.java.com.appsecco.dvja;
+	exports src.main.java.com.appsecco.dvja.controllers;
+	exports src.main.java.com.appsecco.dvja.interceptors;
+	exports src.main.java.com.appsecco.example;
+	exports src.main.java.com.appsecco.dvja.models;
+	exports src.main.java.com.appsecco.dvja.services;
+}
